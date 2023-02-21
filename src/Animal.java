42fd9a1e164125b069dfcb61c977417e19642a4f
Rinat-312 <rkozhobekov88@mail.ru>
2023-02-21 15:09:17 +0300

    Commit 23

---
+++ src/Animal.java
@@ -0,0 +1,8 @@
+public class Animal {
+    public void eat(){
+        System.out.println("Animal is eating");
+    }
+    public void run(){
+        System.out.println("Animal is running");
+    }
+}
