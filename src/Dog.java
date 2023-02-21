d0699be71db6dca460722e9fd815eb3d98762ac5
Rinat-312 <rkozhobekov88@mail.ru>
2023-02-21 15:11:51 +0300

    Commit 23

---
+++ src/Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends Animal {
+    @Override
+    public void eat() {
+        System.out.println("dog is eat");
+    }
+
+    public void bark() {
+        System.out.println("dog is barking");
+    }
+
+}
